e898caab705636d9e26a4098c85b521dc0cf96ff
Terik Hone <terikhone@gmail.com>
2020-07-08 00:59:03 -0600

    Updates

---
+++ precache-manifest.3f89cf921da6de0e773d86d02ef5f474.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77815dc22a84919e1139e4f4cf24aa3f",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "bfc1d4db61b252432e1b",
+    "url": "/clicky-game/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "166360365cadf4ff6d85",
+    "url": "/clicky-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bfc1d4db61b252432e1b",
+    "url": "/clicky-game/static/js/2.bf8bd490.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.bf8bd490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "166360365cadf4ff6d85",
+    "url": "/clicky-game/static/js/main.4d684f8b.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  }
+]);
